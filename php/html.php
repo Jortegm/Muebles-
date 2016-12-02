348789f84653d0cc8e9358bc52ca1d32eef2bb62
Jortegm <juanortegamarin@gmail.com>
2016-12-02 08:48:44 +0100

    subida de css y clase HTML

---
+++ php/html.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*clase creada para añadir las cabeceras de html con un titulo y link de Css y cerrar el html
+*@param $titulo se añade un titulo a la pagina
+*#param $link se utilizara para añadir el link de CSS
+*/
+class Html {
+    public static function AbrirHtml($titulo,$link)
+    {
+        echo "<!DOCTYPE html>";
+        echo "<html><head><title>$titulo</title>";
+        echo "<meta charset='UTF-8'>".$link.">";
+        echo "</head><body>";
+    }
+
+    public static function CerrarHtml()
+    {
+        echo "</body></html>";
+    }
+}
